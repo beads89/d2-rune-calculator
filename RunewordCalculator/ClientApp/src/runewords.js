9705bea2bd5471defe19790de9040f057d70876e
Brandon Eads <brandon181989@hotmail.com>
2021-12-10 13:37:45 -0800

    begin adding runewords by lvlreq high-low

--- RunewordCalculator/ClientApp/src/runewords.js
+++ RunewordCalculator/ClientApp/src/runewords.js
@@ -2,13 +2,40 @@
 
 let runewords = [
   {
-    name: "",
-    gearType: "",
-    lvlReq: int,
-    runes: [""],
-    stats: ""
+    name: "Breath of the Dying",
+    sockets: 6,
+    gearType: "All Ranged and Melee Weapons",
+    lvlReq: 69,
+    runes: ["Vex", "Hel", "El", "Eld", "Zod", "Eth"],
+    stats: ["50% Chance To Cast Level 20 Poison Nova When You Kill An Enemy", "Indestructible", "+60% Increased Attack Speed", "+350-400% Enhanced Damage", "-25% Target Defense", "+50 To Attack Rating", "+200% Damage To Undead", "+50 To Attack Rating Against Undead", "7% Mana Stolen Per Hit", "12-15% Life Stolen Per Hit", "Prevent Monster Heal", "+30 TO All Attributes", "+1 To Light Radius", "Requirements -20%"]
   }, {
-    
-  }
+    name: "Pride",
+    sockets: 4,
+    gearType: "Polearms",
+    lvlReq: 67,
+    runes: ["Cham", "Sur", "Io", "Lo"],
+    stats: ["25% Chance To Cast Level 17 Fire Wall When Struck", "Level 16-20 Concentration Aura When Equipped", "260-300% Bonus To Attack Rating", "+(Player Lvl)% Damage To Demons", "Adds 50-280 Lightning Damage", "20% Deadly Strike", "Hit Blinds Target", "Freezes Target +3", "+10 To Vitality", "Replenish Life +8", "(1.875 * Player Lvl)% Extra Gold From Monsters"]
+  }, {
+    name: "Hand of Justice",
+    sockets: 4,
+    gearType: "All Ranged and Melee Weapons",
+    lvlReq: 67,
+    runes: ["Sur", "Cham", "Amn", "Lo"],
+    stats: ["100% Chance To Cast Level 36 Blaze When You Level-Up", "100% Chance To Cast Level 48 Meteor When You Die", "Level 16 Holy Fire Aura When Equipped", "+33% Increased Attack Speed", "+280-330% Enhanced Damage", "Ignores Target's Defense", "-20% To Enemy Fire Resistance", "7% Life Stolen Per Hit", "20% Deadly Strike", "Hit Blinds Target", "Freezes Target +3"]
+  }, {
+    name: "Doom",
+    sockets: 5,
+    gearType: "Axes, Hammers and Polearms",
+    lvlReq: 67,
+    runes: ["Hel", "Ohm", "Um", "Lo", "Cham"],
+    stats: ["5% Chance To Cast Level 18 Volcano On Striking", "Level 12 Holy Freeze Aura When Equipped", "+2 To All Skills", "+45% Increased Attack Speed", "+330-370% Enhanced Damage", "-40-60% To Enemy Cold Resistance", "50% Deadly Strike", "25% Chance of Open Wounds", "Prevent Monster Heal", "Freezes Target +3", "Requirements -20%"]
+  }, {
+    name: "Last Wish",
+    sockets: 6,
+    gearType: "Axes, Hammers and Swords",
+    lvlReq: 65,
+    runes: ["Jah", "Mal", "Jah", "Sur", "Jah", "Ber"],
+    stats: ["6% Chance To Cast Level 11 Fade When Struck", "10% Chance To Cast Level 18 Life Tap On Striking", "20% Chance To Cast Level 20 Charged Bolt On Attack", "Level 17 Might Aura When Equipped", "+330-375% Enhanced Damage", "Ignore Target's Defense", "60-70% Chance of Crushing Blow", "Prevent Monster Heal", "Hit Blinds Target", "(0.5 * Player Lvl)% Chance of Getting Magic Items"]
+  }, {}
 
 ]
