248a499cea9eee8b3345bbd7bc9958aba6336e8a
Brandon Eads <brandon181989@hotmail.com>
2021-12-22 14:21:06 -0800

    begin index.js finish farm in-game

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { runewords } from './js/runewords.js';
 
+$(document).ready(function() {
+  $("#gearSlot").submit(function(event) {
+    event.preventDefault();
+    const gearInput = $("#gearSlot").val();
+    const runeInput = $("#")
+  })
+})
