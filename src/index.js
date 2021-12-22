fb61c45736397a074e15970502c3ad0e3e55a63a
Brandon Eads <brandon181989@hotmail.com>
2021-12-22 15:39:12 -0800

    remove projectjs, rename inputwidth, try map to display object array

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import { runewords } from './js/runewords.js';
 $(document).ready(function() {
   $("#gearSlot").submit(function(event) {
     event.preventDefault();
-    const gearInput = $("#gearSlot").val();
-    const runeInput = $("#")
-  })
-})
+    // const gearInput = $("#gearSlot").val();
+    const runeInput = $(".rune-input").val();
+    let runeWordOutput = runewords[runewords.map(function (runesHas) {return runesHas.runes;}).indexOf(runeInput)];
+    console.log(runeWordOutput);
+  });
+});
